c13cd29a8663156dbb934af1581857c819bca1d4
GiuseppeDalessio7 <g.dalessio03gmail.com>
2023-11-17 19:25:55 +0100

    Print To page

--- vite-boolfolio/src/App.vue
+++ vite-boolfolio/src/App.vue
@@ -1,9 +1,62 @@
 <script >
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+
+export default {
+  Name: 'App',
+  data() {
+
+    return {
+      myurl: 'http://127.0.0.1:8000/',
+      api_Boolfolio: 'api/projects',
+      projects: [],
+
+    }
+  },
+
+
+  methods: {
+
+  },
+  mounted() {
+
+    axios.get(this.myurl + this.api_Boolfolio)
+      .then(response => {
+        console.log(response);
+        this.projects = response.data.result
+      }).catch(err => {
+        console.error(err);
+      })
+
+  }
+
+
+};
+
 </script>
 
+
 <template>
-  <h1 class="text-center">ciaooooooooooo</h1>
+  <section>
+    <div class="container">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-2 mt-2">
+        <div class="col" v-for="project in  projects ">
+          <div class="card">
+            <img width="300" :src="this.myurl + 'storage/' + project.cover_image" alt="">
+            <div class="card-body">
+              <h4>{{ project.title }}</h4>
+              <h6>{{ project.description }}</h6>
+
+            </div>
+          </div>
+        </div>
+        <div>
+
+
+        </div>
+      </div>
+    </div>
+
+  </section>
 </template>
 
 <style></style>
