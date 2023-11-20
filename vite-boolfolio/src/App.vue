4d27700a54a47f96e5e2328f25d32d943219fb23
GiuseppeDalessio7 <g.dalessio03gmail.com>
2023-11-20 16:05:46 +0100

    Try to components

--- vite-boolfolio/src/App.vue
+++ vite-boolfolio/src/App.vue
@@ -41,35 +41,12 @@ export default {
 
 <template>
   <section>
+    <!-- <AppHeader></AppHeader>
+    <router-view></router-view> -->
     <div class="container">
       <h1 class="text-center">All my project</h1>
       <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-2 mt-2">
-        <div class="col" v-for="project in    projects   ">
-          <div class="card">
-            <img width="300" :src="this.myurl + 'storage/' + project.cover_image" alt="">
-            <img :src="project.cover_image" alt="">
-
-
-            <div class="card-body">
-              <h4>{{ project.title }}</h4>
-              <h6>{{ project.description }}</h6>
-              <div>
-                <small>Link Git Hub:</small> <a href="#"> {{ project.github }}</a>
-              </div>
-              <div>
-                <small>Link to Project:</small> <a href="#">{{ project.r_link }} </a>
-              </div>
-
-              {{ project.types }}
-
-              {{ project.technologies }}
-            </div>
-          </div>
-        </div>
-        <div>
-
-
-        </div>
+        <ProjectCard v-for="project in projects" />
       </div>
     </div>
 
--- vite-boolfolio/src/components/ProjectCard.vue
+++ vite-boolfolio/src/components/ProjectCard.vue
@@ -3,12 +3,40 @@
 export default {
     name: 'ProjectCard',
 
-
+    props: {
+        project: Object,
+        myurl: String,
+    }
 
 }
 
 </script>
 
-<template></template>
+<template>
+    <div class="col">
+        <div class="card">
+            <img width="300" :src="this.myurl + 'storage/' + project.cover_image" alt="">
+            <img :src="project.cover_image" alt="">
+
+
+            <div class="card-body">
+                <h4>{{ project.title }}</h4>
+                <h6>{{ project.description }}</h6>
+                <div>
+                    <small>Link Git Hub:</small> <a href="#"> {{ project.github }}</a>
+                </div>
+                <div>
+                    <small>Link to Project:</small> <a href="#">{{ project.r_link }} </a>
+                </div>
+
+                {{ project.types }}
+
+                {{ project.technologies }}
+            </div>
+        </div>
+    </div>
+    <div>
+    </div>
+</template>
 
 <style ></style>
